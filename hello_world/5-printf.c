46c108d1521477a1f04bcd5e30adc404023d0fcc
TheHaruYoshida <santiagovidarte@gmail.com>
2022-09-27 08:10:58 -0700

    task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+i#include <stdio.h>
+/**
+* main - main prints "with proper grammar, but the outcome is a piece of art,"
+*
+* Description: printf prints a string
+* Return: A string of words and add a newline
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
