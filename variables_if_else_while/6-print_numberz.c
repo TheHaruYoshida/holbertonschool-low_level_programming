a406139ffe6f68c27a1ec559ed2a6c7e8e7f6178
TheHaruYoshida <santiagovidarte@gmail.com>
2022-09-28 09:40:05 -0700

    add task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - prints a random number from 0 to 9
+* Description: Prints a random number taken from a variable
+* Return: 0
+ */
+int main(void)
+{
+		int c = 48;
+
+		for (c = 48 ; c <= 57 ; c++)
+			putchar(c);
+		putchar('\n');
+		return (0);
+}
