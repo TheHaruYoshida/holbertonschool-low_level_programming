a21d3067000e00adca8dc00ab0a06f560478d7a2
TheHaruYoshida <santiagovidarte@gmail.com>
2022-09-29 05:39:51 -0700

    add task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "_putchar.c"
+/**
+  * main - prints '_putchar'
+  *
+  * Description: prints '_putchar' and a new line leterr by letter
+  * Return: always 0
+  */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
