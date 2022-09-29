0d92e6b2fa712a22543a81cab1da8d31c024b9c1
TheHaruYoshida <santiagovidarte@gmail.com>
2022-09-29 06:01:49 -0700

    add task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * main - Prints the alphabet in lowercase, folowed by a new line
+  *
+  * Description:Prints each character of the alphabet from "a"
+  * up to "z" in lowercase
+  * Return: 0
+  */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
