1aac78802d7956d0502dac785649bb7873c6de66
TheHaruYoshida <santiagovidarte@gmail.com>
2022-09-29 06:35:48 -0700

    hotfix abc

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include "_alphabet.c"
 /**
-  * main - Prints the alphabet in lowercase, folowed by a new line
+  * abc - Prints the alphabet in lowercase, folowed by a new line
   *
-  * Description:Prints each character of the alphabet from "a"
+  * Description: Prints each character of the alphabet from "a"
   * up to "z" in lowercase
   * Return: 0
   */
-int main(void)
+int abc(void)
 {
 	print_alphabet();
 	return (0);
