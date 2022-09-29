a3b132689e8d165bbe077c6649dd654558f90a53
TheHaruYoshida <santiagovidarte@gmail.com>
2022-09-29 07:23:10 -0700

    add task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "_putchar.c"
+#include "_islower.c"
+/**
+ * checker - check the code.
+ *
+ * Return: 0 or 1 depending of the number that checks.
+ */
+int checker(void)
+{
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
