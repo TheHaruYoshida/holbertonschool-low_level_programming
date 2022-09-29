86929f2e574a8a02000845c346dbb4bb0d65ccab
TheHaruYoshida <santiagovidarte@gmail.com>
2022-09-29 07:05:47 -0700

    add task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "_print_alphabet_x10.c"
+/**
+ * abcx10 - prints alphabet 10 times
+ *
+ * Return: Always 0.
+ */
+int abcx10(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
