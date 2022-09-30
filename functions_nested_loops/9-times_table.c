c25394f86362ae435174f5ecea77a297bf254688
TheHaruYoshida <santiagovidarte@gmail.com>
2022-09-30 06:22:05 -0700

    add task 9 files

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "_times_table.c"
+/**
+ * timestable - prints the 9 times table, starting with 0
+ *
+ * Description: prints the 9 ties table, starting with 0
+ * Return: Always 0.
+ **/
+int timestable(void)
+{
+	times_table();
+	return (0);
+}
