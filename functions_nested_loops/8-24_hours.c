0645e0da6bf4a1e8dba57550cb21ac72408b7aec
TheHaruYoshida <santiagovidarte@gmail.com>
2022-09-30 05:38:13 -0700

    task 8 hotfix betty style

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include "_24_hours.c"
 /**
-   counter - prints each of the day
+  * counter - prints each of the day
   *
   * Description: prints each hour since 00:00 up to 23:59
   * Return: Always 0.
   **/
 int counter(void)
 {
-	    jack_bauer();
-	        return (0);
+	jack_bauer();
+	return (0);
 }
