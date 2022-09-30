951c3659fe94c059a071a0fd411cd570eefcdf6d
TheHaruYoshida <santiagovidarte@gmail.com>
2022-09-30 07:30:43 -0700

    add task 11 files

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "_print_to_98.c"
+/**
+ * printer - check the code
+ *
+ * Description: prints each integer from n up to 98
+ * Return: Always 0.
+ **/
+int printer(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
+	return (0);
+}
