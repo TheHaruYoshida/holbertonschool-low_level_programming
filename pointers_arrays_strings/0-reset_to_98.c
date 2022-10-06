6008069db81d6ade7f23a72b62c16b2d25f8190c
TheHaruYoshida <santiagovidarte@gmail.com>
2022-10-06 08:19:34 -0700

    hotfix betty task 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /*
+   * reset_to_98 - updates the parameter of n
+   * @n: points up to the value of n
    *
-   *
-   *
-   *
+   * Description: takes a pointer to an int as parameter
+   * and updates the value it points to to 98.
+   * Return:void
    */
 void reset_to_98(int *n)
 {
