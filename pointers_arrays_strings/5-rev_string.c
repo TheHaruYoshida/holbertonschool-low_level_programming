4c921b0b2495d367c67fe9186bff5a0d743c9374
TheHaruYoshida <santiagovidarte@gmail.com>
2022-10-13 05:19:46 -0700

    add tasks

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,22 +8,16 @@
   * of the other extreme..
   * Return: void.
   */
-
-void rev_string(char *s)
-{
-	int i, j, count = 0;
-	char *c, tmp;
-
-	c = s;
-	while (s[count] != '\0')
-		count++;
-	for (j = 1; j < count; j++)
-		c++;
-	for (i = 0; i < count / 2; i++)
-	{
-		tmp = s[i];
-		s[i] = *c;
-		*c = tmp;
-		c--;
-	}
+void rev_string(char *s){
+    char tmp;
+    int count = 0;
+    int i = 0;
+    while(s[count]!='\0'){
+        count++;
+    }
+    for (i=0; i<count/2 ; i++){
+        tmp = s[i];
+        s[i] = s[count-i-1];
+        s[count-i-1] = tmp;
+    }
 }
