fe3f4f438a11d27f3f92661dead545fde8b89acd
TheHaruYoshida <santiagovidarte@gmail.com>
2022-10-17 07:04:48 -0700

    recursion tasks

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * _strlen_recursion - count number of characters on a string..
+  * @s: String to count.
+  *
+  * Return: Number of characters.
+  */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
