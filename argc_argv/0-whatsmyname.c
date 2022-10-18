c6557511c64f17003ceb56c67ab477d3a708e0f1
TheHaruYoshida <santiagovidarte@gmail.com>
2022-10-18 07:08:32 -0700

    hotfix task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main (__attribute__((unused)) int argc, char *argv[])
+/**
+ * main - Prints its name
+ *
+ * @argv: contains the name
+ * @argc: unused
+ * Description: prints its name, followed by a new line.
+ * Return: always 0
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-	printf ("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
