afc78dd203fd39cf9750af98a7ddd27ada42623c
TheHaruYoshida <santiagovidarte@gmail.com>
2022-10-18 09:17:32 -0700

    hotfix task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
+  * main - multiplies two numbers.
+  * @argc: Count number of argument
+  * @argv: Array of strings (argument)
   *
-  *
-  *
+  * Return: always 0
   */
 int main(__attribute__((unused)) int argc, char **argv)
 {
@@ -13,10 +15,10 @@ int main(__attribute__((unused)) int argc, char **argv)
 	if (argc < 3)
 	{
 		printf("Error\n");
-		return(0);
+		return (1);
 	}
-	for (i = 1; i <3; i++)
+	for (i = 1; i < 3; i++)
 		j *= atoi(argv[i]);
 	printf("%d\n", j);
-	return(0);
+	return (0);
 }
