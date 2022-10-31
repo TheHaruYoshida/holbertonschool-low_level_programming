5979f43c6943578ad49bcbd8061f9aec77ed7058
TheHaruYoshida <santiagovidarte@gmail.com>
2022-10-31 12:08:48 -0700

    hotfix betty

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,4 +1,3 @@
-#include <stddef.h>
 #ifndef variadic_functions_h
 #define variadic_functions_h
 
@@ -6,9 +5,4 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-typedef struct list
-{
-	char ch;
-	char *spec;
-} list_t;
 #endif
