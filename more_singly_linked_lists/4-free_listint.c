647e135f7c1b06c7483072c98a6ffd98a4c9400b
TheHaruYoshida <santiagovidarte@gmail.com>
2022-11-20 09:17:39 -0800

    add task 4

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * free_listint - Frees a listint_t list.
+ * @head: A pointer to the head of the listint_t list to be freed.
+ */
+void free_listint(listint_t *head)
+{
+	if (head)
+	{
+		free_listint(head->next);
+		free(head);
+	}
+}
