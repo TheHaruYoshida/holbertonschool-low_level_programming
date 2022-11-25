2852d3e2c3abe822c4757c55f805e1550ed27639
TheHaruYoshida <santiagovidarte@gmail.com>
2022-11-25 04:05:01 -0800

    add task 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * read_textfile -  reads a text file and prints it to the POSIX stdout.
+  * @filename: the name of the file to read and print.
+  * @letters: the number of letters it should read and print.
+  *
+  * Return: The number of letters it could read and print, 0 if the file
+  * cannot be opened or read, if is NULL, or if write fails
+  */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, wr, rd;
+	char *buf;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(letters + 1);
+	if (!buf)
+		return (0);
+	rd = read(fd, buf, letters);
+	if (rd == -1)
+		return (0);
+	buf[letters + 1] = '\0';
+	close(fd);
+	wr = write(STDOUT_FILENO, buf, rd);
+	if (wr == -1)
+		return (0);
+	free(buf);
+	return (rd);
+}
