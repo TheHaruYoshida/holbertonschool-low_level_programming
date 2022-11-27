2d8c519ab4982faddc83b2c6b558d551ac378934
TheHaruYoshida <santiagovidarte@gmail.com>
2022-11-27 08:16:32 -0800

    add task 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+  * main - copies the content of a file to another file.
+  * @ac: the number of arguments passed.
+  * @av: the pointer to an array of arguments passed.
+  *
+  * Return: allways 0.
+  */
+
+int main(int ac, char **av)
+{
+	int fd, fd2, wr, rd = 1024;
+	char *buf = malloc(1024);
+
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP |
+			S_IWGRP | S_IROTH);
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		close(fd), exit(99);
+	}
+	while (rd == 1024)
+	{
+		rd = read(fd, buf, 1024);
+		if (rd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+		wr = write(fd2, buf, rd);
+		if (wr < rd)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+
+	}
+	if (close(fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+	if (close(fd2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+	return (0);
+}
