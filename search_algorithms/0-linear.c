1805cf1a07776987d16dc85524fd3af375992855
TheHaruYoshida <santiagovidarte@gmail.com>
2023-01-08 17:24:24 -0800

    hotfix betty task 0

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -12,10 +12,10 @@ int linear_search(int *array, size_t size, int value)
 	size_t i = 0;
 
 	if (!array)
-		return(-1);
-	while(i < size)
+		return (-1);
+	while (i < size)
 	{
-		printf("Value checked [%ld] = [%d]\n", i, array[i]);
+		printf("Value checked[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 		i++;
