822da31373766cdc9f686db73d9a519d07e3a1fd
TheHaruYoshida <santiagovidarte@gmail.com>
2023-01-08 17:19:58 -0800

    adding task 0, README, and header

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+/**
+ * linear_search - seaches for a value in an array
+ * @array: array of integers
+ * @size: size of the array
+ * @value: value that is needed to search
+ *
+ * Return: the index of the value searched if success or -1 if fail.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	while(i < size)
+	{
+		printf("Value checked [%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
